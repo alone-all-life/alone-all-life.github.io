51df7cb2b4f338541085444efc37d256e23cec77
wenxuan-xia <wenxuan.xia@hotmail.com>
2015-02-16 01:28:15 +0800

    change the page into Project-style

--- js/main-page-hw-loader.js
+++ js/main-page-hw-loader.js
@@ -1,43 +1,29 @@
 $(document).ready(
 	function() {
 		url = window.location.href
-		var hw_no = url.search("[0-9]")
-		if (hw_no === -1) {
-			hw_no = 2
+		var content = url.search("#")
+		if (content>=0) {
+			q = url.split("#")
+			content = q[1]
 		} else {
-			hw_no = url[hw_no]
+			content = "team"
 		}
-		if (hw_no == 0) {
-			load_ref()
-		} else {
-			load_homework(hw_no)
-		}
+
+		load_page(content)
 	}
 )
 
-function load_homework(hw_no) {
-	url = "./hw-page/hw_" + hw_no + ".html"
-	$.get(
-			url,
-			{},
-			function(o) {
-				document.getElementById('homework-loader').innerHTML = o
-				if (hw_no==2) {
-					load_stu_score()
-				}
-			},
-			"html"
-		);
-}
 
-function load_ref() {
-	url = "./ref.html"
+
+function load_page(content) {
+	url = "./pages/" + content + ".html"
 	$.get(
 			url,
 			{},
 			function(o) {
-				document.getElementById('homework-loader').innerHTML = o
+				document.getElementById('content-loader').innerHTML = o
 			},
 			"html"
 		);
+	
 }
